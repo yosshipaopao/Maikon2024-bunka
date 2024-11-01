afdd1d70d4d6e1d83ff022524c7b85d46a814f59
yosshipaopao <yosshipaopao@gmail.com>
2024-11-01 16:53:03 +0900

    up

--- src/Sensor.h
+++ src/Sensor.h
@@ -7,10 +7,13 @@
 #define rep(i, n) for (int i = 0; i < (int)(n); i++)
 // センサー
 #define PIN_SIZE 5
-#define PINS {A2, A3, A4, A5, A6}
+#define PINS {A3, A4, A5, A6, A7}
 #define PIN_REP_CNT 5
-#define SENSOR_THRESHOLDS {220,220,200,180,180}
-const int SENSOR_THRESHOLD = 205;
+#define SENSOR_THRESHOLDS {185,170,175,175,185}
+#define COLOR_SENSOR_THRESHOLD_WHITE 50
+#define COLOR_SENSOR_THRESHOLDS {55,55}
+#define COLOR_SENSOR_HIST_SIZE 7
+#define COLOR_SENSOR_HIST_THRESHOLD 7
 
 class Sensor
 {
@@ -18,13 +21,27 @@ private:
     // センサーのpin番号
     int pins[PIN_SIZE] = PINS;
     int thresholds[PIN_SIZE] = SENSOR_THRESHOLDS;
+    int color_thresholds[2] = COLOR_SENSOR_THRESHOLDS;
+
+    int color_raw_hist_idx = 0;
+    int color_raw_hist[2][2][COLOR_SENSOR_HIST_SIZE];
+
+    void new_color_raw_hist(int i,int j,int k,int l){
+        color_raw_hist[0][0][color_raw_hist_idx] = i;
+        color_raw_hist[0][1][color_raw_hist_idx] = j;
+        color_raw_hist[1][0][color_raw_hist_idx] = k;
+        color_raw_hist[1][1][color_raw_hist_idx] = l;
+        color_raw_hist_idx++;
+        color_raw_hist_idx %= COLOR_SENSOR_HIST_SIZE;
+    }
+
     // pinの判断基準
 public:
     // 読み込んだデータの一時保存場所
     int data[PIN_SIZE];
 #ifdef USE_COLOR_SENSOR
-    Adafruit_TCS34725 c_sensor_1 = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_24MS, TCS34725_GAIN_16X);
-    Adafruit_TCS34725 c_sensor_2 = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_24MS, TCS34725_GAIN_16X);
+    Adafruit_TCS34725 c_sensor_1 = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_2_4MS, TCS34725_GAIN_16X);
+    Adafruit_TCS34725 c_sensor_2 = Adafruit_TCS34725(TCS34725_INTEGRATIONTIME_2_4MS, TCS34725_GAIN_16X);
     uint16_t sensor1_color[4];
     uint16_t sensor2_color[4];
 #endif
@@ -53,6 +70,7 @@ void set()
 #ifdef USE_COLOR_SENSOR
     c_sensor_1.getRawData(&sensor1_color[0], &sensor1_color[1], &sensor1_color[2], &sensor1_color[3]);
     c_sensor_2.getRawData(&sensor2_color[0], &sensor2_color[1], &sensor2_color[2], &sensor2_color[3]);
+    new_color_raw_hist(sensor1_color[0],sensor1_color[1],sensor2_color[0],sensor2_color[1]);
 #endif
 }
 // 値の取得(基準を超えてるかチェック)
@@ -60,6 +78,47 @@ bool get(int i)
 {
     return data[i] > thresholds[i];
 };
+
+unsigned int get_state(){
+    unsigned int state = 0;
+    rep(i, PIN_SIZE){
+        state |= get(i) << i;
+    }
+    return state;   
+}
+    int detect_color(int s){
+        int sumr = 0, sumg = 0;
+        rep(i, COLOR_SENSOR_HIST_SIZE)sumr += color_raw_hist[s][0][i];
+        rep(i, COLOR_SENSOR_HIST_SIZE)sumg += color_raw_hist[s][1][i];
+        if(true){
+            Serial.print("sumr: ");
+            Serial.print(sumr);
+            Serial.print(" sumg: ");
+            Serial.println(sumg);
+        }
+        if(sumr + sumg > 1000)return 0;// white
+        if(sumg - 20 > sumr)return 1;// green
+        return 2; // black
+    }
+    // 0 others
+    // 1 black black
+    // 2 white black
+    // 3 black white
+    // 4 green white
+    // 5 white green
+    // 6 green green
+    int get_color_state(){
+        int now_state = 0;
+        int c_1 = detect_color(0);
+        int c_2 = detect_color(1);
+        if(c_1 == 2 && c_2 == 2)now_state = 1;
+        else if(c_1 == 0 && c_2 == 2)now_state = 2;
+        else if(c_1 == 2 && c_2 == 0)now_state = 3;
+        else if(c_1 == 1 && c_2 == 0)now_state = 4;
+        else if(c_1 == 0 && c_2 == 1)now_state = 5;
+        else if(c_1 == 1 && c_2 == 1)now_state = 6;
+        return now_state;
+    }
 // Serialで値（上位二けたを表示)
 void debug()
 {
@@ -79,19 +138,31 @@ void debug_raw(){
     Serial.println();
 }
 void debug_color(){
+    Serial.print("now color state: ");
+    Serial.println(get_color_state());
+}
+void debug_color_raw(int mode=0){
 #ifdef USE_COLOR_SENSOR
-    Serial.print("sensor1: ");
-    Serial.print(sensor1_color[0]);
-    Serial.print(" ");
-    Serial.print(sensor1_color[1]);
-    Serial.print(" ");
-    Serial.println(sensor1_color[2]);
-    Serial.print("sensor2: ");
-    Serial.print(sensor2_color[0]);
-    Serial.print(" ");
-    Serial.print(sensor2_color[1]);
-    Serial.print(" ");
-    Serial.println(sensor2_color[2]);
+    if(mode == 0 || mode == 1){
+        Serial.print("sensor1: ");
+        Serial.print(sensor1_color[0]);
+        Serial.print(" ");
+        Serial.print(sensor1_color[1]);
+        Serial.print(" ");
+        Serial.print(sensor1_color[2]);
+        Serial.print(" ");
+        Serial.println(sensor1_color[3]);
+    }
+    if(mode == 0 || mode == 2){
+        Serial.print("sensor2: ");
+        Serial.print(sensor2_color[0]);
+        Serial.print(" ");
+        Serial.print(sensor2_color[1]);
+        Serial.print(" ");
+        Serial.print(sensor2_color[2]);
+        Serial.print(" ");
+        Serial.println(sensor2_color[3]);
+    }
 #endif
 }
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <Arduino.h>
 #include "Wire.h"
 #include "SPI.h"
+#include "Servo.h"
 #include "Motor.h"
 
 #define USE_COLOR_SENSOR
@@ -24,16 +25,19 @@ const int RIGHT_D1 = 9;
 const int RIGHT_D2 = 10;
 const int RIGHT_DA = 8;
 
+const int SERVO_PIN_0 = 0;
+const int SERVO_PIN_1 = 1;
+
 // nani
 const int STANDBY = 16;
 
 //// 微調整用の変数
 // 通常走行の速度
-const int RUN_SP = 30;
+const int RUN_SP = 40;
 // ゆっくり曲がるときの速度
-const int HALF_STOP_SP = 15;
+const int HALF_STOP_SP = 20;
 // 強く曲がるときの速度
-const int STOP_SP = -10;
+const int STOP_SP = 0;
 // sensor
 const int COLOR_THRESHOLD1 = 10;
 const int COLOR_THRESHOLD2 = 10;
@@ -49,6 +53,8 @@ Motor motor_rd(RIGHT_DA, RIGHT_D1, RIGHT_D2);
 
 Sensor sensor;
 
+Servo servo_0;
+Servo servo_1;
 
 void motor_setup()
 {
@@ -56,6 +62,7 @@ void motor_setup()
   motor_ld.setup();
   motor_ru.setup();
   motor_rd.setup();
+  digitalWrite(STANDBY, HIGH);
 }
 
 void left_motor(int speed)
@@ -85,12 +92,11 @@ void turn(int l_speed, int r_speed)
 }
 
 
-
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
+  pinMode(18,INPUT_PULLUP);
   sensor.setup();
-  //motor_setup();
-  //digitalWrite(STANDBY, HIGH);
+  motor_setup();
 }
 
 // 前回どっち側検出したか
@@ -101,69 +107,200 @@ unsigned long l_color_last_ditect = 0;
 unsigned long r_color_last_ditect = 0;
 unsigned long last_ditect = 0;
 
+int states[10];
+int state_index = 0;
+void new_state(int state)
+{
+  states[state_index++] = state;
+  state_index %= 10;
+}
+int sum_state = 0;
+int get_sum_state()
+{
+  sum_state = 0;
+  rep(i, 10)sum_state += states[i];
+  return sum_state;
+}
 
+int color_states[COLOR_SENSOR_HIST_SIZE];
+int color_states_idx =0;
+int set_color_states(int state)
+{ 
+  int old_state = color_states[color_states_idx];
+  color_states[color_states_idx] = state;
+  color_states_idx++;
+  color_states_idx %= COLOR_SENSOR_HIST_SIZE;
+
+  if(old_state == 6 && state == 1) return 1;
+  else if (old_state == 5&&(state == 2||state==1))return 2;
+  else if (old_state == 4&&(state == 3||state==1)) return 3;
+  return 0;
+}
+void reset_color_states(){
+  rep(i,COLOR_SENSOR_HIST_SIZE)color_states[i] = 0;
+}
+
+void force_turn_90(int to){
+  if(to==1)turn(HALF_STOP_SP,-HALF_STOP_SP);
+  else turn(-HALF_STOP_SP,HALF_STOP_SP);
+  delay(1200);
+  rep(i,10)states[i] = 1;
+   while (true)  
+  {
+    sensor.set();
+    int state = sensor.get_state();
+    if (state == 0b01110 || state == 0b01100 || state == 0b00110 || state == 0b00100 || state == 0b11111)new_state(0);
+    else new_state(1);
+    get_sum_state();
+    if(sum_state < 3)break;
+  }
+  rep(i,10)states[i] = 0;
+}
+int sw_hist[100];
+int sw_hist_idx = 0;
+int sum_sw_hist(){
+  int sum = 0;
+  rep(i,100)sum += sw_hist[i];
+  return sum;
+}
 void loop() {
   sensor.set(); // センサーの値を読み取ります。
-  //sensor.debug();
-  sensor.debug_raw();
-  //sensor.debug_color();
-  delay(100);
-  return;
+  int color_sensor_result = set_color_states(sensor.get_color_state());
 
-  // 真ん中が同時についたら直進する
-  if (sensor.get(2) && !sensor.get(1) && !sensor.get(3))
-  {
-    straight(RUN_SP);
-    before = 0;
-    last_ditect = millis();
-  }
-  // 真ん中の右だけだったら少しだけ右に
-  else if (sensor.get(3))
-  {
-    turn(RUN_SP, HALF_STOP_SP);
-    before = 0;
-    last_ditect = millis();
-  }
-  // 真ん中の左だけだったら少しだけ左に
-  else if (sensor.get(1))
-  {
-    turn(HALF_STOP_SP, RUN_SP);
-    before = 0;
-    last_ditect = millis();
-  }
-  // 右端だったら強く右に曲がる
-  else if (sensor.get(4))
-  {
-    turn(RUN_SP, STOP_SP);
-    before = 1;
-    last_ditect = millis();
-  }
-  // 左端だったら強く左に曲がる
-  else if (sensor.get(0))
-  {
-    turn(STOP_SP, RUN_SP);
-    before = -1;
-    last_ditect = millis();
-  }
-  // 5つが検出されてないとき
-  else
-  {
-    // 前回と同じ動きをする
-    if (before > 0)
-    {
-      turn(RUN_SP, STOP_SP);
-    }
-    else if (before < 0)
-    {
-      turn(STOP_SP, RUN_SP);
-    }
-    else
-    {
-      // どっちでもないときは直進
-      straight(RUN_SP);
-    }
-  }
-  delay(10);
+  sw_hist[sw_hist_idx++]=!digitalRead(18);
+  sw_hist_idx %= 100;
   
+  if(sum_sw_hist() > 80){
+    straight(-RUN_SP);
+    delay(1500);
+    turn(-RUN_SP,RUN_SP);
+    delay(1100);
+    straight(RUN_SP);
+    delay(3000);
+    turn(RUN_SP,-RUN_SP);
+    delay(1100);
+    straight(RUN_SP);
+    delay(3000);
+    turn(RUN_SP,-RUN_SP);
+    delay(1100);
+    rep(i,10)states[i] = 0;
+    rep(i,100)sw_hist[i] = 0;
+  }
+  
+  //Serial.print("color(state,result) :");
+  //Serial.println(sensor.get_color_state());
+  /*
+  Serial.print(" ");
+  Serial.println(color_sensor_result);*/
+  //if(color_sensor_result != 0)Serial.println("color sensor detect");
+  //sensor.debug();
+  //sensor.debug_raw();
+  //sensor.debug_color();
+  //sensor.debug_color_raw(0);
+  //sensor.detect_color(0);
+  //delay(100);
+  //return;
+  //straight(RUN_SP);
+  //delay(10);
+  
+  //Serial.print(sensor.detect_color(0));
+  //Serial.print(" ");
+  //Serial.println(sensor.detect_color(1));
+  //Serial.println();
+  //return;
+  
+  get_sum_state();
+  //Serial.println(sum_state);
+
+  if(color_sensor_result!=0){
+    straight(HALF_STOP_SP);
+    delay(1500);
+    if(color_sensor_result==1){
+      Serial.println("180 turn");
+      force_turn_90(0);
+      Serial.println("90 turn");
+      force_turn_90(0);
+      Serial.println("180 fin");
+    }else if(color_sensor_result == 2){
+      Serial.println("90 turn");
+      force_turn_90(0);
+    }else if(color_sensor_result == 3){
+      Serial.println("-90 turn");
+      force_turn_90(1);
+    }
+    reset_color_states();
+  }
+  // 真ん中が同時についたら直進する
+  switch (sensor.get_state())
+  {
+  case 0b11111:
+  case 0b11110:
+  case 0b01111:
+  case 0b01110:
+  case 0b00100:
+    straight(RUN_SP);
+    new_state(0);
+    break;
+  case 0b01100:
+    turn(RUN_SP, HALF_STOP_SP);
+    new_state(1);
+    break;
+  case 0b00110:
+    turn(HALF_STOP_SP, RUN_SP);
+    new_state(-1);
+    break;
+  case 0b10000:
+    turn(RUN_SP, STOP_SP);
+    new_state(2);
+    break;
+  case 0b00001:
+    turn(STOP_SP, RUN_SP);
+    new_state(-2);
+    break;
+  case 0b11100:
+    turn(RUN_SP, STOP_SP);
+    new_state(3);
+    break;
+  case 0b11000:
+    turn(RUN_SP, STOP_SP);
+    new_state(3);
+    break;
+  case 0b00111:
+    turn(STOP_SP, RUN_SP);
+    new_state(-30);
+    break;
+  case 0b00011:
+    turn(STOP_SP, RUN_SP);
+    new_state(-10);
+    break;
+  case 0b00000:
+    new_state(0);
+    Serial.println("!!!");
+    get_sum_state();
+    straight(HALF_STOP_SP);
+    delay(700);
+    if(sum_state > 30){
+      turn(HALF_STOP_SP,-HALF_STOP_SP);
+    }else if(sum_state < -30){
+      turn(-HALF_STOP_SP,HALF_STOP_SP);
+    }else{
+      straight(HALF_STOP_SP);
+    }
+    rep(i,10)states[i] = 1;
+    while (true)  
+    {
+      sensor.set();
+      int state = sensor.get_state();
+      if (state == 0b01110 || state == 0b01100 || state == 0b00110 || state == 0b00100 || state == 0b11111)new_state(0);
+      else new_state(1);
+      get_sum_state();
+      if(sum_state < 2)break;
+    }
+    rep(i,10)states[i] = 0;
+    break;
+  default:
+    straight(RUN_SP);
+    break;
+  }
   ///// line trace end
 }
