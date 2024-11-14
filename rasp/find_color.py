e9d2861e06157409c5adc27936e4c042346c8f19
yosshipaopao <yosshipaopao@gmail.com>
2024-11-14 17:18:54 +0900

    fu

--- rasp/find_color.py
+++ rasp/find_color.py
@@ -1,6 +1,12 @@
 import cv2
+#import cv2.typing as cvt
 import numpy as np
+import numpy.typing as npt
 
-def find_red(frame:cv2.typing.MatLike)->cv2.typing.MatLike:
+def find_red(frame:npt.NDArray,debug_image:bool=False)->npt.NDArray:
     img_mask = cv2.inRange(frame, np.array([0,0,125,0]), np.array([100,100,255,255]))
+    if debug_image:
+        cimg = frame.copy()
+        cimg[img_mask != 0] = cimg[len(cimg)//2][len(cimg[0])//2]
+        cv2.imshow("red",cimg)
     return img_mask
--- rasp/main.py
+++ rasp/main.py
@@ -14,7 +14,7 @@ def main():
         "size": (1640, 1232)
     }))
     camera.start()
-    ser = comn.setup()
+    #ser = comn.setup()
     time.sleep(2)
     gpio = servo.setup()
 
@@ -23,26 +23,26 @@ def main():
         frame = frame[:,:1000]
         frame = cv2.resize(frame,None,fx=0.5,fy=0.5)
 
-        red = find_color.find_red(frame)
+        red = find_color.find_red(frame,debug_image=False)
         print(red.sum())
 
-        circles = circle.find_circle(frame)
-        for i in circles:
-            if len(circles[0]) > 4:
-                comn.motor(ser,0,0,debug=True)
-            else:
-                for i in circles[0,:]:
-                    x = 50
-                    y = 50
-                    x += (i[1] - 320) / 30
-                    y -= (i[1] - 320) / 30
-                    comn.motor(ser,x,y,debug=True)
+        circles = circle.find_circle(frame,debug_image=True)
+        if len(circles) < 4:
+            for i in circles:
+                x = 50
+                y = 50
+                x += (i[1] - 320) / 30
+                y -= (i[1] - 320) / 30
+                #comn.motor(ser,x,y,debug=True)
     
-        key =cv2.waitKey(10)
+        key = cv2.waitKey(10)
         if key == 27:
             break
     
     
     camera.stop()
-    ser.close()
+    #ser.close()
     cv2.destroyAllWindows()
+
+if __name__ == "__main__":
+    main()
